f672fae782be542d53145747a2c192fb4255bcbe
yijunwanghaha@163.com <yijunwanghaha@163.com>
2019-01-06 18:34:18 +0800

    Initial commit

---
+++ src/main/java/com/sp/test/TestSpApplication.java
@@ -0,0 +1,15 @@
+package com.sp.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TestSpApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/sp/test/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.sp.test.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hi")
+    public String sayHi(){
+        return "hello world";
+    }
+}
